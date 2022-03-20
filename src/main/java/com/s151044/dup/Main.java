409ce3e236f5464fbc97ac84ddb4f610ee5da3f3
151044 <151044@std.stpaulscollege.edu.hk>
2022-03-21 00:05:42 +0800

    Remove spurious throws

--- src/main/java/com/s151044/dup/Main.java
+++ src/main/java/com/s151044/dup/Main.java
@@ -1,13 +1,9 @@
 package com.s151044.dup;
 
-import org.xml.sax.SAXException;
 import picocli.CommandLine;
 
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+    public static void main(String[] args) {
         CommandLine cmd = new CommandLine(new Command());
         cmd.execute(args);
     }
